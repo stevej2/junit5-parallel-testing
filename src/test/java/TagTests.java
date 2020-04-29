a8c755a0fa545182bacf3f89232ab5f1e9e5373f
Spjohn2 <62660050+Spjohn2@users.noreply.github.com>
2020-04-29 08:55:08 +0100

    first commit

--- src/test/java/TagTests.java
+++ src/test/java/TagTests.java
@@ -1,31 +1,50 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
-@Execution(ExecutionMode.CONCURRENT)
+import java.time.Duration;
+import java.time.Instant;
+
+// @Execution(ExecutionMode.CONCURRENT)
 public class TagTests
 {
+    static Instant startTime;
+
+    @BeforeAll
+    public static void startTime() {
+        startTime = Instant.now();
+    }
+
+    @AfterAll
+    public static void endTime() {
+        Instant end = Instant.now();
+        Duration timeElapsed = Duration.between(startTime, end);
+        System.out.println(timeElapsed.toSeconds() + " seconds elapsed");
+    }
+
     @Test
-    public void testTag1()
-    {
+    public void testTag1() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("testTag1");
     }
 
     @Test
-    public void testTag2()
-    {
+    public void testTag2() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("testTag2");
     }
 
     @Test
-    public void testTag3()
-    {
+    public void testTag3() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("testTag3");
     }
 
     @Test
-    public void testTag4()
-    {
+    public void testTag4() throws InterruptedException {
+        Thread.sleep(5000);
         System.out.println("testTag4");
     }
 }
